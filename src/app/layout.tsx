a225a1e01b9b4ed2b211dc66a3031e26b98c9b38
ajeeshRS <ajeeshrs79@gmail.com>
2025-01-08 18:42:21 +0530

    added script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from 'sonner';
 import SessionWrapper from '@/components/providers/session-provider';
 import Footer from '@/components/layouts/Footer';
 import ReduxProvider from '@/components/providers/redux-provider';
-import ClarityScript from '@/components/ClarityScript';
+import Script from 'next/script';
 export const metadata: Metadata = {
   title: 'Remoteez',
   description: 'Your Remote Job Finder.',
@@ -25,7 +25,15 @@ export default function RootLayout({
       >
         <ReduxProvider>
           <SessionWrapper>
-            <ClarityScript />
+          <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.TRACKING_ID}");
+          `}
+            </Script>
             <Navbar />
             {children}
             <Footer />
